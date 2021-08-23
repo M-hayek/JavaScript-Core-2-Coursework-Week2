3562ab3a2498399069c492aaf15202effa733ac5
MahaM <mahahayek.mh@gmail.com>
2021-08-23 13:23:29 +0100

    C-D completed

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,23 +1,47 @@
-function readingList(books) {
-  // Write your code here...
+function readingList(books, booksCover) {
+  //Write your code here...
+  let content = document.querySelector ("#content");
+  let unorderedList = document.createElement ("ul");
+  content.appendChild(unorderedList);
+  unorderedList.style.display= "flex";
+  unorderedList.style.display="wrap";
+  
+
+  for (let i = 0; i<books.lenth; i++){
+     let addBook = document.createElement ("li");
+     unorderedList.appendChild (addBook);
+     addBook.style.listStyleType = 'none';
+		 addBook.style.width = '300px';
+	   addBook.style.height = '300px';
+	   addBook.style.padding = '25px';
+	
+     let bookTitle = document.createElement ("p");
+     addBook.appendChild(bookTitle);
+     p.style.marginBottom = '40px';
+     p.textContent = books[i].title + " by " + books[i].authors;
+
+
+     let bookImage = document.createElement ("img");
+     addBook.appendChild(bookImage);
+     bookImage.style.maxWidth = '200px';
+		 bookImage.style.maxHeight = '200px';
+	   bookImage.src = booksCover[i];
+
+     if (books[i].alreadyRead) {
+			addBook.style.backgroundColor = 'green';
+		} else addBook.style.backgroundColor = 'red';
+
+  }
+
 }
 
-const books = [
-  {
-    title: "The Design of Everyday Things",
-    author: "Don Norman",
-    alreadyRead: false
-  },
-  {
-    title: "The Most Human Human",
-    author: "Brian Christian",
-    alreadyRead: true
-  },
-  {
-    title: "The Pragmatic Programmer",
-    author: "Andrew Hunt",
-    alreadyRead: true
-  }
+
+
+const booksCover = [
+	'https://static.epub.vn/epubvn-image/optimized-cover1570413642597-artworks-000442818750-sxidwx-t500x500.jpg',
+	'https://machinatorium.files.wordpress.com/2012/10/human-cover1.jpg',
+	'https://images-na.ssl-images-amazon.com/images/I/51cUVaBWZzL._SS600_.jpg',
 ];
 
-readingList(books);
+
+readingList(books, booksCover);
--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,22 @@
 function todoList(todos) {
   // Write your code here...
+
+  let content = document.querySelector ("#content");
+  let list = document.createElement ("ul");
+  content.appendChild(list);
+
+  todos.forEach ((add) => { let entry = document.createElement("li");
+  list.appendChild(entry);
+  entry.innerText=add.todo;
+  
+  entry.addEventListener ("click", () => {
+    if (entry.style.textDecoration === "none") {
+      entry.style.textDecoration = "line-through  black";
+    } else {
+      entry.style.textDecoration = "none";
+    }
+  });
+});
 }
 
 const todos = [
